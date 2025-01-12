ca014dcef4b0914709d01c607e976207ea9333c7
Hikmal Falah <ikmalfalah@gmail.com>
2025-01-12 22:04:19 +0700

    Update {style}: title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <div className="card-title mb-3">
-            <h3 className="mt-3 mb-4 text-light"><i className="bx bx-list-ul fs-1 align-middle"></i> Todo List</h3>
+            <h3 className="mt-3 mb-4 text-light"><i className="bx bx-list-ul fs-1 align-middle"></i> Todo List.<small className="fs-6">v2</small></h3>
         </div>
     )
 }
